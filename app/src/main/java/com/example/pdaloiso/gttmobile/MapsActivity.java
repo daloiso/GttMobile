a8823006b223e16b3808fc3aa1e1151e19eaf817
unknown <pasquale.daloiso@gmail.com>
2015-05-14 11:19:24 +0200

    studio ws

--- app/src/main/java/com/example/pdaloiso/gttmobile/MapsActivity.java
+++ app/src/main/java/com/example/pdaloiso/gttmobile/MapsActivity.java
@@ -2,9 +2,18 @@ package com.example.pdaloiso.gttmobile;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.pdaloiso.gttmobile.database.SqlController;
+import com.example.pdaloiso.gttmobile.model.ModelParsedWithGson;
 import com.example.pdaloiso.gttmobile.model.Percorso;
+import com.example.pdaloiso.gttmobile.ws.GsonRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -12,6 +21,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MapsActivity extends FragmentActivity {
 
@@ -90,5 +102,33 @@ public class MapsActivity extends FragmentActivity {
                         (latlon1.longitude + latlon2.longitude) / 2),
                 15));
 
+        /*
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://api.androidhive.info/volley/person_object.json";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+            public void onResponse(JSONObject response) {
+                try {
+                    String email = response.getString("email");
+                    JSONObject phone = response.getJSONObject("phone");
+                    String home = phone.getString("home");
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }, null);
+
+        GsonRequest gsonObjRequest = new GsonRequest<ModelParsedWithGson>(Request.Method.GET, url,
+                ModelParsedWithGson.class, null, new Response.Listener<ModelParsedWithGson>() {
+            @Override
+            public void onResponse(ModelParsedWithGson response) {
+
+            }
+        },null);
+
+        queue.add(jsObjRequest);
+        */
     }
+
 }
---
+++ app/src/main/java/com/example/pdaloiso/gttmobile/model/ModelParsedWithGson.java
@@ -0,0 +1,7 @@
+package com.example.pdaloiso.gttmobile.model;
+
+/**
+ * Created by pdaloiso on 14/05/2015.
+ */
+public class ModelParsedWithGson {
+}
